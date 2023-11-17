c06a53c8c08e49e203372c1d71a763c659e25dd9
Kudresdss <95304220+Kudresdss@users.noreply.github.com>
2023-11-18 01:20:36 +0300

    makefile install edit

--- src/install.sh
+++ src/install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 DIR=""
-echo "Введите директорию для установки приложения"
+echo "Введите директорию для установки приложения (для установки в текущую директорию нажмите enter)"
 read DIR
 
 if [ "$DIR" == "" ]; then
-    cmake --build build
-    cp cp build/SmartCalculator.app/Contents/MacOs/SmartCalculator SmartCalculator
+    cmake -B build
+    make -C build
+    cp build/SmartCalculator.app/Contents/MacOs/SmartCalculator SmartCalculator
 else
-    cmake --build $DIR/build
+    cmake -B $DIR/build
+    make -C $DIR/build
+    cp $DIR/build build/SmartCalculator.app/Contents/MacOs/SmartCalculator SmartCalculator
 fi
